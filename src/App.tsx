3659f9c770a6afac964ee9cc92de5304dec100fe
alvaroprado00 <72304152+alvaroprado00@users.noreply.github.com>
2023-09-11 14:58:48 +0200

    Routing + Checkout page
    This commit includes react routing functionality + the first steps towards building the Checkout page.

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,33 @@ import { HomePage } from './layouts/HomePage/HomePage';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 
 export const App = () => {
   return (
-    <div>
-      <Navbar/>
-      {/*<HomePage/>*/}
-      <SearchBooksPage/>
-      <Footer/>
+    <div className='d-flex flex-column min-vh-100'>
+      <div className='flex-grow-1'>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to='/home' />
+          </Route>
+          <Route path="/home" exact>
+            <HomePage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+          <Route path="/checkout/:bookId">
+            <BookCheckoutPage />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
     </div>
 
- 
+
   );
 }
 
