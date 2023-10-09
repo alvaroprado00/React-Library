9f561c53076e9d847b4decb13f245a29cc3e8ede
alvaroprado00 <72304152+alvaroprado00@users.noreply.github.com>
2023-10-09 17:16:28 +0200

    History functionality
    This commit includes the History entity, the History repository and a modification in the return book method in the book service class to create entries in the DB.

--- src/layouts/ShelfPage/ShelfPage.tsx
+++ src/layouts/ShelfPage/ShelfPage.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react"
+import { HistoryPage } from "./components/HistoryPage"
 import { Loans } from "./components/Loans"
 
 export const ShelfPage = () => {
+
+    const [historyClick, setHistoryClick] = useState(false);
 
 
     return (
@@ -9,13 +13,13 @@ export const ShelfPage = () => {
             <div className="mt-3">
                 <nav>
                     <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                        <button className="nav-link active" id="nav-loans-tab" data-bs-toggle='tab'
+                        <button onClick={()=>{setHistoryClick(false)}} className="nav-link active" id="nav-loans-tab" data-bs-toggle='tab'
                             data-bs-target='#nav-loans' type="button" role="tab" aria-controls='nav-loans'
                             aria-selected='true'>
                             Loans
                         </button>
 
-                        <button className="nav-link" id="nav-history-tab" data-bs-toggle='tab'
+                        <button  onClick={()=>{setHistoryClick(true)}} className="nav-link" id="nav-history-tab" data-bs-toggle='tab'
                             data-bs-target='#nav-history' type="button" role="tab" aria-controls='nav-history'
                             aria-selected='false'>
                             History
@@ -31,7 +35,7 @@ export const ShelfPage = () => {
                     <div className="tab-pane fade" id='nav-history' role="tabpanel"
                         aria-labelledby="nav-history-tab">
                         <p>
-                            History
+                           {historyClick? <HistoryPage/>: <></>}
                         </p>
                     </div>
 
